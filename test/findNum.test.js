5e2217a42a94b8b048a321d3605b84a500a48b4c
Victor Lee <myleevictor@gmail.com>
2018-03-04 21:09:46 +0800

    test folder added

---
+++ test/findNum.test.js
@@ -0,0 +1,15 @@
+var findFirstNum = require("../src/findNum.js");
+
+describe("Find first non-consecutive num in array", () => {
+  // it('findFirstNum([array]) must return array.length of 2', () => {
+  //     expect(findFirstNum([1,2,3])).toEqual(3);
+  // });
+
+  it("elements in findFirstNum([1,2,3,4,5,6]) are all consecutive", () => {
+    expect(findFirstNum([1, 2, 3, 4, 5, 6])).toBeNull();
+  });
+
+  it("The first NON-consecutive num in findFirstNum([1,2,4,5,6])", () => {
+    expect(findFirstNum([1, 2, 4, 5, 6])).toEqual(4);
+  });
+});
