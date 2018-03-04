789101ac27b4acb1248dfdd5c4b8a57d94504902
Victor Lee <myleevictor@gmail.com>
2018-03-04 21:09:21 +0800

    moved test.js to test folder and refactored toBeNull() still returns undefined

--- src/findNum.js
+++ src/findNum.js
@@ -1,6 +1,5 @@
 function findFirstNum(arr) {
-  
-    for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] + 1 !== arr[i + 1]) {
       return arr[i + 1];
     }
