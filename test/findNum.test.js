03864452e8f02efec9318ed4652b2cd158734ebe
Victor Lee <myleevictor@gmail.com>
2018-06-20 12:21:28 +0800

    edited require variable name to const

--- test/findNum.test.js
+++ test/findNum.test.js
@@ -1,4 +1,4 @@
-var findFirstNum = require("../src/findNum.js");
+const findFirstNum = require("../src/findNum.js");
 
 describe("Find first non-consecutive num in array", () => {
   it('findFirstNum([1,2,4,5]) must return 4', () => {
